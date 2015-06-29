1780b5f3cfbc5560536595ce2da7688cc12c29bf
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-06-29 14:25:06 -0300

    joystick as remote_control object test

--- test/joystick.cpp
+++ test/joystick.cpp
@@ -1,39 +1,37 @@
 #include <stdio.h>
 
+#include "../remote_command.h"
 #include "../joystick.h"
 #include "../rovermisc.h"
 
 int main(int argc, char *argv[]){
-	rover::joystick Joystick;
+	rover::remote_command *Joystick = new rover::joystick();
 	rover::command_t command;
 
 
-	if(Joystick.opened()){
-		printf("Joystick abierto\n");
-	}
-
-	//Joystick.test();
-	
 	//Rover State
 	int tvel = 0, avel = 0;
-	
+
 	while(1){
-		Joystick.get_command(command);
+		if(Joystick->get_command(command)){
 
-		if(command.type == rover::VEL_A_COMMAND){
-			avel = (int)command.value*100.0;
-		}
-		if(command.type == rover::VEL_R_COMMAND){
-			tvel = (int)command.value*100.0;
-		}
-		if(command.type == rover::EXIT_COMMAND){
-			break;
-		}
+			if(command.type == rover::VEL_A_COMMAND){
+				avel = (int)(command.value*100.0);
+			}
+			if(command.type == rover::VEL_R_COMMAND){
+				tvel = (int)(command.value*100.0);
+			}
+			if(command.type == rover::EXIT_COMMAND){
+				break;
+			}
 
-		printf("Estado: \n\tvelocida tangencial: %d\n\tvelocidad angular: %d\n", tvel, avel);
+			printf("Estado: \n\tvelocida tangencial: %d\n\tvelocidad angular: %d\n", tvel, avel);
+		}
 
 		rover::wait(10000);
 	}
+
+	delete Joystick;
 
 	return 0;
 }
