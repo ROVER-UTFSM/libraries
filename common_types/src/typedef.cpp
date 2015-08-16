d242ef99256e2f1a515c974d5811adf4bf54f123
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-08-15 23:46:14 -0300

    refactoring of folder

---
+++ common_types/src/typedef.cpp
@@ -0,0 +1,173 @@
+#include "typedef.h"
+
+namespace rover{
+
+	// polar_vec_t implementation ###############
+
+	polar_vec_t& polar_vec_t::operator+(polar_vec_t &v){
+		this->r += v.r;
+		this->a += v.a;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator+(float e){
+		this->r += e;
+		this->a += e;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator-(polar_vec_t &v){
+		this->r -= v.r;
+		this->a -= v.a;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator-(float e){
+		this->r -= e;
+		this->a -= e;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator*(float e){
+    	this->r *= e;
+		this->a *= e;
+		return *this;
+    }
+    polar_vec_t& polar_vec_t::operator/(float e){
+    	this->r /= e;
+		this->a /= e;
+		return *this;
+	}
+	polar_vec_t& polar_vec_t::operator+=(polar_vec_t &v){
+		this->r += v.r;
+		this->a += v.a;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator+=(float e){
+		this->r += e;
+		this->a += e;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator-=(polar_vec_t &v){
+		this->r -= v.r;
+		this->a -= v.a;
+		return *this;
+	}
+    polar_vec_t& polar_vec_t::operator-=(float e){
+		this->r -= e;
+		this->a -= e;
+		return *this;
+	}
+	polar_vec_t& polar_vec_t::operator*=(float e){
+		this->r *= e;
+		this->a *= e;
+		return *this;
+	}
+	polar_vec_t& polar_vec_t::operator/=(float e){
+		this->r /= e;
+		this->a /= e;
+		return *this;
+	}
+
+	// vector_state implementation ##############
+
+	vector_state::vector_state():
+		pos({0.0,0.0}),
+		spd({0.0,0.0}),
+		acc({0.0,0.0}){}
+
+	vector_state& vector_state::operator+(vector_state &vs){
+		this->pos += vs.pos;
+		this->spd += vs.spd;
+		this->acc += vs.acc;
+		return *this;
+	}
+	vector_state& vector_state::operator+(polar_vec_t &v){
+		this->pos += v;
+		this->spd += v;
+		this->acc += v;
+		return *this;
+	}
+    vector_state& vector_state::operator+(float e){
+    	this->pos += e;
+		this->spd += e;
+		this->acc += e;
+		return *this;
+    }
+    vector_state& vector_state::operator-(vector_state &vs){
+    	this->pos -= vs.pos;
+		this->spd -= vs.spd;
+		this->acc -= vs.acc;
+		return *this;
+    }
+    vector_state& vector_state::operator-(polar_vec_t &v){
+    	this->pos -= v;
+		this->spd -= v;
+		this->acc -= v;
+		return *this;
+    }
+    vector_state& vector_state::operator-(float e){
+    	this->pos -= e;
+		this->spd -= e;
+		this->acc -= e;
+		return *this;
+    }
+    vector_state& vector_state::operator*(float e){
+    	this->pos *= e;
+		this->spd *= e;
+		this->acc *= e;
+		return *this;
+    }
+    vector_state& vector_state::operator/(float e){
+    	this->pos /= e;
+		this->spd /= e;
+		this->acc /= e;
+		return *this;
+    }
+
+    vector_state& vector_state::operator+=(vector_state &vs){
+		this->pos += vs.pos;
+		this->spd += vs.spd;
+		this->acc += vs.acc;
+		return *this;
+	}
+	vector_state& vector_state::operator+=(polar_vec_t &v){
+		this->pos += v;
+		this->spd += v;
+		this->acc += v;
+		return *this;
+	}
+    vector_state& vector_state::operator+=(float e){
+    	this->pos += e;
+		this->spd += e;
+		this->acc += e;
+		return *this;
+    }
+    vector_state& vector_state::operator-=(vector_state &vs){
+    	this->pos -= vs.pos;
+		this->spd -= vs.spd;
+		this->acc -= vs.acc;
+		return *this;
+    }
+    vector_state& vector_state::operator-=(polar_vec_t &v){
+    	this->pos -= v;
+		this->spd -= v;
+		this->acc -= v;
+		return *this;
+    }
+    vector_state& vector_state::operator-=(float e){
+    	this->pos -= e;
+		this->spd -= e;
+		this->acc -= e;
+		return *this;
+    }
+    vector_state& vector_state::operator*=(float e){
+    	this->pos *= e;
+		this->spd *= e;
+		this->acc *= e;
+		return *this;
+    }
+    vector_state& vector_state::operator/=(float e){
+    	this->pos /= e;
+		this->spd /= e;
+		this->acc /= e;
+		return *this;
+    }
+
+}
---
+++ sensors/src/joystick_test.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "typedef.h"
+#include "rovermisc.h"
+#include "../headers/joystick.h"
+
+int main(int argc, char *argv[]){
+	rover::command *Joystick = new rover::joystick();
+	rover::command_t command;
+
+
+	//Rover State
+	int tvel = 0, avel = 0;
+
+	if( ((rover::joystick*)Joystick)->failed() ){
+		printf("No se encontro joystick\n");
+		return -1;
+	}
+
+	while(1){
+		if(Joystick->get_command(command)){
+
+			if(command.type == rover::VEL_A_COMMAND){
+				avel = (int)(command.value*100.0);
+			}
+			if(command.type == rover::VEL_R_COMMAND){
+				tvel = (int)(command.value*100.0);
+			}
+			if(command.type == rover::EXIT_COMMAND){
+				break;
+			}
+
+			printf("Estado: \n\tvelocida tangencial: %d\n\tvelocidad angular: %d\n", tvel, avel);
+		}
+
+		rover::wait(10000);
+	}
+
+	delete Joystick;
+
+	return 0;
+}
