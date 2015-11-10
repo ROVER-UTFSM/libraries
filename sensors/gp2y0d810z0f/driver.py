63afa03b0c07d47807b7d6e4c1a90e0e3a7f4e84
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-10 15:12:12 -0300

    ?

--- sensors/gp2y0d810z0f/driver.py
+++ sensors/gp2y0d810z0f/driver.py
@@ -4,3 +4,5 @@
 # stand & first measure time : 6[ms] no measurment
 # Low  : [20, 100] [mm]             }
 # High : [0,20[ v ]100,inf[ [mm]    } Histeresis : medir !!!!!
+# tiempo máximo de refresco : 3.77[ms] (Typ: 2.56[ms])
+#
