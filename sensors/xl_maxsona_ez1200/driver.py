c549808d180b0a68861f4f9f904838fb867f74cd
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-10 22:37:15 -0300

    sonar python driver

---
+++ sensors/xl_maxsona_ez1200/driver.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+__author__ = 'Cristobal Ramirez'
+
+# Lectura de distancia en PWM
+
+import RPi.GPIO as gpio
+
+class xl_maxsonar_ez2100:
+
+	def __inti__(self):
+		pass
+
+	def debug(self):
+
+
+
+if __name__ == "__main__":
+	print("Hi!")
