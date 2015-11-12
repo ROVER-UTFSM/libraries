4c6f07dd39cc2abc81bf091bedfce35b66b6ae05
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-12 15:16:23 -0300

    test

--- sensors/picamera/camera_example.py
+++ sensors/picamera/camera_example.py
@@ -3,5 +3,18 @@
 
 __author__="Cristobal Ramirez"
 
+import time
+import picamera
+
 if __name__ == "__main__":
-	pass
+	import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+
+    time.sleep(1)
+
+    camera.capture('test.jpg')
+	
