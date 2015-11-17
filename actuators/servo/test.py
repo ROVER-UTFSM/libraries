9ed16a7d724cd2898340d8104b24067d232bea2b
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-17 16:41:24 -0300

    test servo

--- actuators/servo/test.py
+++ actuators/servo/test.py
@@ -1,1 +1,18 @@
 #! /usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+
+if __name__ == "__main__":
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+	
+	sleep(2)
+	GPIO.output(12, GPIO.HIGH)
+	sleep(1)
+	GPIO.output(12, GPIO.LOW)
+	sleep(3)
+	GPIO.output(12, GPIO.HIGH)
+	sleep(2)
+
+	GPIO.cleanup(12)
