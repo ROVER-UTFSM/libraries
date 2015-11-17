38706ab460d2facd70a5e6b00e0395b5bc9331ff
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-17 16:49:51 -0300

    printing

--- actuators/servo/test.py
+++ actuators/servo/test.py
@@ -6,13 +6,17 @@ import time
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+	print("Low")
 	
 	time.sleep(2)
 	GPIO.output(12, GPIO.HIGH)
+	print("HIgh")
 	time.sleep(1)
 	GPIO.output(12, GPIO.LOW)
+	print("Low")
 	time.sleep(3)
 	GPIO.output(12, GPIO.HIGH)
+	print("HIgh")
 	time.sleep(2)
 
 	GPIO.cleanup(12)
