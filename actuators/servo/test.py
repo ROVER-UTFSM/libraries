072b5470c9a332b128f3fe0d5d42fb8ecbf62505
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-17 16:45:10 -0300

    time.sleep()

--- actuators/servo/test.py
+++ actuators/servo/test.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
 	
-	sleep(2)
+	time.sleep(2)
 	GPIO.output(12, GPIO.HIGH)
-	sleep(1)
+	time.sleep(1)
 	GPIO.output(12, GPIO.LOW)
-	sleep(3)
+	time.sleep(3)
 	GPIO.output(12, GPIO.HIGH)
-	sleep(2)
+	time.sleep(2)
 
 	GPIO.cleanup(12)
