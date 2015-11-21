0df08503d4195e9318f72f8cbbc8cd79da9c765c
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-21 16:55:55 -0300

    measuring omega of encoder

--- sensors/encoder_dfrobot/driver.c
+++ sensors/encoder_dfrobot/driver.c
@@ -1,32 +1,62 @@
 #include <stdio.h>
+#include <wiringPi.h>
 #include <time.h>
-#include <sys/types.h>
+#include <types.h>		// clockid_t
 
-#define NTIMES 10
+#define PIN 1							// PIN for encoder
+#define EDGE_DETECTION INT_EDGE_BOTH	// Using both edges
 
-long factor(long x){
-	long r = 1;
-	int i;
-	for(i=0;i<x;i++) x *= i;
-	return r;
-}
+
+/*** Global variables ***/
+
+/* State of driver */
+volatile struct timespec t0, t1;
+volatile clockid_t c = CLOCK_REALTIME;			// Real-time clock (not CPU time)
+volatile int last_timer = 0;					// 0: t0 has last time measurment ; 1: t1 has last time measurment
+
+/* Measurements */
+const int n_ranuras = 10;
+const float theta_min = 360.0 / (2.0*n_ranuras);	// 2* when using both edges
+const float omega = 0.0;
+
+/* Definitions */
+void pulse_period(void);	// Interruption
+
 
 int main(){
-	long tprom = 0;
-	clockid_t c = CLOCK_REALTIME;
-	struct timespec t0, t1;
+	/* Wiring setups */
+	wiringPiSetup();
+	wiringPiISR(PIN, EDGE_DETECTION, &pulse_period);
+	// TODO : capture errors
+
+	/* Clock setup */
 	clock_settime(c, &t0);
 	clock_settime(c, &t1);
+	clock_gettime(c, &t0);	// initial value
+	clock_gettime(c, &t1);	// initial value
 
-	int i;
-	for(i=0;i<NTIMES;i++){
-		clock_gettime(c, &t0);
-		factor(10);
-		clock_gettime(c, &t1);
-		tprom += t1.tv_nsec-t0.tv_nsec;
-	}
-	printf("Delta: %.1f[ns]\n", tprom*1.0 / NTIMES);
-
+	/* Infinite Loop */
+	while(1);
 
 	return 0;
+}
+
+
+/* Pulse Interruption -- PIN 1 (12 BOARD) */
+void pulse_period(void){
+	/* Hipotesis : Despues de un canto de subida siempre sigue un canto de bajada */
+
+	if(last_timer){
+		clock_gettime(c, &t1);
+		printf("omega : %.2f\n",  theta_min / (t1.tv_nsec - t0.tv_nsec));
+
+		last_timer = 0;		// Pulse goes DOWN
+	}else{
+		clock_gettime(c, &t0);
+		printf("omega : %.2f\n",  theta_min / (t0.tv_nsec - t1.tv_nsec));
+
+		last_timer = 1;		// Pulse goes UP
+	}
+
+	/* Publish message ROS */
 }
