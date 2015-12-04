193549dd1be1a8da6e04f0f5a31b6932087057e7
Rover <proyecto.rover@gmail.com>
2015-12-04 13:32:00 +0000

    interruption state change

--- sensors/encoder_dfrobot/driver.c
+++ sensors/encoder_dfrobot/driver.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <wiringPi.h>
 #include <time.h>
-#include <types.h>		// clockid_t
+#include <sys/types.h>						// for clockid_t
 
 #define PIN 1							// PIN for encoder
-#define EDGE_DETECTION INT_EDGE_BOTH	// Using both edges
-
+#define EDGE_DETECTION INT_EDGE_BOTH				// Using both edges
+#define PI 3.1415
 
 /*** Global variables ***/
 
 /* State of driver */
 volatile struct timespec t0, t1;
-volatile clockid_t c = CLOCK_REALTIME;			// Real-time clock (not CPU time)
+volatile clockid_t c    = CLOCK_MONOTONIC;			// Real-time clock (not CPU time)
 volatile int last_timer = 0;					// 0: t0 has last time measurment ; 1: t1 has last time measurment
 
 /* Measurements */
-const int n_ranuras = 10;
-const float theta_min = 360.0 / (2.0*n_ranuras);	// 2* when using both edges
-const float omega = 0.0;
+const int n_ranuras   = 10;
+const float theta_min = 2*PI / (2.0*10);			// 2* when using both edges
+const float omega     = 0.0;
 
 /* Definitions */
-void pulse_period(void);	// Interruption
+void pulse_period(void);					// Interruption
 
 
 int main(){
@@ -44,18 +44,18 @@ int main(){
 
 /* Pulse Interruption -- PIN 1 (12 BOARD) */
 void pulse_period(void){
-	/* Hipotesis : Despues de un canto de subida siempre sigue un canto de bajada */
+	/* Hipotesis : Despues de un canto de subida siempre sigue un canto de bajada :O */
 
 	if(last_timer){
 		clock_gettime(c, &t1);
-		printf("omega : %.2f\n",  theta_min / (t1.tv_nsec - t0.tv_nsec));
+		printf("delta t : %.3f\n", t1.tv_sec-t0.tv_sec + (t1.tv_nsec - t0.tv_nsec)/1000000000.0);
 
-		last_timer = 0;		// Pulse goes DOWN
+		last_timer = 0;		// t0 has the last time
 	}else{
 		clock_gettime(c, &t0);
-		printf("omega : %.2f\n",  theta_min / (t0.tv_nsec - t1.tv_nsec));
+		printf("delta t : %.3f\n", t0.tv_sec-t1.tv_sec + (t0.tv_nsec - t1.tv_nsec)/1000000000.0);
 
-		last_timer = 1;		// Pulse goes UP
+		last_timer = 1;		// t1 has the last time
 	}
 
 	/* Publish message ROS */
