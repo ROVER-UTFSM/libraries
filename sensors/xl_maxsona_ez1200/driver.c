16939c66fd3e711cf30b52d0115952789f03aef3
Rover <proyecto.rover@gmail.com>
2015-12-04 13:56:46 +0000

    -ah-

--- sensors/xl_maxsona_ez1200/driver.c
+++ sensors/xl_maxsona_ez1200/driver.c
@@ -13,7 +13,7 @@ volatile struct timespec t0, t1;				// t0: pulse start time t1: pulse end time
 volatile const clockid_t c = CLOCK_REALTIME;	// Real-time clock (not CPU time)
 
 /* Measurements */
-const int K_cm_to_ns = 58000					// Linear transformation : x[cm] = t[ns] / 58000
+const int K_cm_to_ns = 58000;					// Linear transformation : x[cm] = t[ns] / 58000
 
 
 /*** Definitions ***/
