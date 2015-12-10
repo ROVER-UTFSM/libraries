105fd85e40e2ce7886bc691b528de44c427220af
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-12-10 10:18:47 -0300

    test PWM rpi

---
+++ actuators/chasis.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+
+#include <wiringPi.h>
+
+#define PWM_PIN 			1				// unico pin para pwm
+#define PWM_CLOCK_DIVISOR	16				// 19.2[MHz] /16 = 1.2[MHz]
+#define PWM_RANGE			1024			// 1.2[MHz] /1024 = 1.171875[KHz]
+
+int main(){
+	/* Wiring setups */
+	wiringPiSetup();
+	pinMode(PWM_PIN, PWM_OUTPUT);
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetClock(PWM_CLOCK_DIVISOR);
+	pwmSetrRange(PWM_RANGE);
+
+
+:POR_SIEMPRE
+	int i=0;
+	for(i=0; i<PWM_RANGE; i+=20){
+		pwmWrite(PWM_PIN, i);
+		usleep(10000);
+	}
+
+	goto POR_SIEMPRE
+	/**/
+
+	return 0;
+}
